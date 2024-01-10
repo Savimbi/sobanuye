48afb7f70c0021f95bf0e471098b8867b86e7bce
Dominique Savio Dukuzumuremyi <dominique_savio_dukuzumuremyi@traas.co.jp>
2024-01-10 11:51:47 +0900

    Add movie model and its unittest

--- moviereview/urls.py
+++ moviereview/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from movie import views as movieViews
+import os
+
+from moviereview import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +28,5 @@ urlpatterns = [
     path("about/", movieViews.about, name="about"),
     path("signup/", movieViews.signup, name='signup')
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
