0e022c51c295b6cc2bc1b0759ab111cf2199fcb5
Dominique Savio Dukuzumuremyi <dominique_savio_dukuzumuremyi@traas.co.jp>
2024-01-16 17:43:23 +0900

    Fix url file

--- moviereview/urls.py
+++ moviereview/urls.py
@@ -18,6 +18,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from movie import views as movieViews
+from django.conf.urls import include
 import os
 
 from moviereview import settings
